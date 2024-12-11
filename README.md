5ef50d8b37d16c9be469174dbcc5b6053d714183
Agam Patel <colabwithagam@gmail.com>
2024-12-11 10:40:30 +0000

    Setup Done

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 1. `conda create -n medibot`
 2. `conda activate medibot`
 3. `pip install -r requirements.txt`
+4. `pip install -e .`
+5. `python app.py`
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Agam Patel',
+    author_email= 'colabwithagam@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
